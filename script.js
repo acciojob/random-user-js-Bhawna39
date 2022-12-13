05fe68280ae3d077d995533300b1352f5ef1e07c
Bhawna <92748048+Bhawna39@users.noreply.github.com>
2022-12-14 00:02:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
-//your code here
-
-function renderText(domElt, text) {
-    domElt.innerText = text;
-  }
-  
-  function handleWord(text) {
-    const textArr = text.split(" ");
-    let wordCount = 0;
-  
-    for (word of textArr) {
-      if (/[a-zA-Z0-9]/.test(word)) {
-        wordCount += 1;
-      }
-    }
-   return wordCount;
-  }
-  
-  function handleChar(text) {
-    return text.length;
-  }
-  
-  function init() {
-    const textArea = document.querySelector("textarea");
-    const charDOM = document.querySelector("#letterCount");
-    const wordDOM = document.querySelector("#wordCount");
-  
-    textArea.addEventListener("input", (event) => {
-      const text = event.target.value.trim();
-  
-      renderText(wordDOM, handleWord(text));
-      renderText(charDOM, handleChar(text));
+async function fetchdata() {
+  const url = "https://randomuser.me/api/";
+  const data = await fetch(url);
+  const response = await data.json();
+  // console.log(response);
+  return response.results[0];
+}
+const showuser = () => {
+  fetchdata().then((data) => {
+    console.log(data.name.first, data.name.last);
+    console.log(data.picture.large);
+    document.getElementById(
+      "img-box"
+    ).innerHTML = `<img src="${data.picture.large}">`;
+    document.getElementById(
+      "username"
+    ).innerHTML = `${data.name.first} ${data.name.last}`;
+    const ageButton = document.getElementById("age");
+    ageButton.addEventListener("click", () => {
+      console.log(data.dob.age);
+      document.getElementById("output").innerHTML = `<h1>${data.dob.age}<h1>`;
     });
-  }
-  
-  init();
+    const EmailButton = document.getElementById("email");
+    EmailButton.addEventListener("click", () => {
+      console.log(data.email);
+      document.getElementById("output").innerHTML = `<h1>${data.email}<h1>`;
+    });
+    const PhoneButton = document.getElementById("phone");
+    PhoneButton.addEventListener("click", () => {
+      console.log(data.phone);
+      document.getElementById("output").innerHTML = `<h1>${data.phone}<h1>`;
+    });
+  });
+};
+
+showuser();
+const newUser =document.getElementById("getUser");
+newUser.addEventListener("click", () => {
+  showuser();
+});
